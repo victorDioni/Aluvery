31443425a2365e63661aa897f7390e66e99c7aba
victorDioni <victor.dionizio28@gmail.com>
2024-05-16 10:24:29 -0300

    Personalizando os campos do form

--- app/src/main/java/com/victordionizio/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/victordionizio/aluvery/ui/activities/ProductFormActivity.kt
@@ -3,20 +3,28 @@ package com.victordionizio.aluvery.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.victordionizio.aluvery.ui.theme.AluveryTheme
 
 class ProductFormActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
+        setContent {
             AluveryTheme {
                 Surface {
                     ProductFormScren()
@@ -27,38 +35,79 @@ class ProductFormActivity : ComponentActivity() {
 }
 
 @Composable
-fun ProductFormScren(){
-    Column {
-        Text(text = "Criando o produto")
+fun ProductFormScren() {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "Criando o produto",
+            Modifier.fillMaxWidth(),
+            fontSize = 28.sp
+        )
         var url by remember {
             mutableStateOf("")
         }
-        TextField(value = url, onValueChange = {
-            url = it
-        })
+        TextField(
+            value = url,
+            onValueChange = {
+                url = it
+            },
+            Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Url da imagem")
+            },
+        )
 
         var name by remember {
             mutableStateOf("")
         }
-        TextField(value = name, onValueChange = {
-            name = it
-        })
+        TextField(
+            value = name,
+            onValueChange = {
+                name = it
+            },
+            Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Nome")
+            },
+        )
 
-        var price  by remember {
+        var price by remember {
             mutableStateOf("")
         }
-        TextField(value = price , onValueChange = {
-            price  = it
-        })
+        TextField(
+            value = price,
+            onValueChange = {
+                price = it
+            },
+            Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Preço")
+            },
+        )
 
-        var description  by remember {
+        var description by remember {
             mutableStateOf("")
         }
-        TextField(value = description , onValueChange = {
-            description  = it
-        })
+        TextField(
+            value = description,
+            onValueChange = {
+                description = it
+            },
+            Modifier.fillMaxWidth()
+                .heightIn(100.dp),
+            label = {
+                Text(text = "Descrição")
+            },
+        )
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(
+            onClick = { /*TODO*/ },
+            Modifier.fillMaxWidth(),
+        ) {
             Text(text = "Salvar")
         }
     }
@@ -66,7 +115,7 @@ fun ProductFormScren(){
 
 @Preview(showBackground = true)
 @Composable
-fun ProductFormScrenPreview(){
+fun ProductFormScrenPreview() {
     AluveryTheme {
         Surface {
             ProductFormScren()
