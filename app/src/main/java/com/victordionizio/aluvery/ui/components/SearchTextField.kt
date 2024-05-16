500de198dc44919b5d1826b884bd8f9e77f34d2d
victorDioni <victor.dionizio28@gmail.com>
2024-05-16 09:21:21 -0300

    Add comentario

--- app/src/main/java/com/victordionizio/aluvery/ui/components/SearchTextField.kt
+++ app/src/main/java/com/victordionizio/aluvery/ui/components/SearchTextField.kt
@@ -19,6 +19,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.victordionizio.aluvery.ui.theme.AluveryTheme
 
+//SearchTextField - Componente responsavel por atualizar o estado do text
+
 @Composable
 fun SearchTextField(
     searchText: String,
