15c83ab78df3359c56f5d2eed5de0703320f9bc5
victorDioni <victor.dionizio28@gmail.com>
2024-05-16 09:41:16 -0300

    Criando activity de Produto

--- app/src/main/java/com/victordionizio/aluvery/ui/activities/MainActivity.kt
+++ app/src/main/java/com/victordionizio/aluvery/ui/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.victordionizio.aluvery
+package com.victordionizio.aluvery.ui.activities
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
---
+++ app/src/main/java/com/victordionizio/aluvery/ui/activities/ProductFormActivity.kt
@@ -0,0 +1,14 @@
+package com.victordionizio.aluvery.ui.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class ProductFormActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+
+        }
+    }
+}
