8346733ca2011d265336d4eb2c52a5bb13b64163
victorDioni <victor.dionizio28@gmail.com>
2024-05-20 10:51:04 -0300

    Add FAB

--- app/src/main/java/com/victordionizio/aluvery/ui/activities/MainActivity.kt
+++ app/src/main/java/com/victordionizio/aluvery/ui/activities/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.victordionizio.aluvery.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.victordionizio.aluvery.sampladata.sampleSections
 import com.victordionizio.aluvery.ui.screens.HomeScreen
 import com.victordionizio.aluvery.ui.theme.AluveryTheme
@@ -13,17 +22,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            App(onFabClick = {
+                startActivity(Intent(
+                    this,
+                    ProductFormActivity::class.java
+                ))
+            })
         }
     }
 }
 
 @Composable
-fun App() {
+fun App(onFabClick: () -> Unit = ({})) {
     AluveryTheme {
         Surface {
-            HomeScreen(sampleSections)
-
+            Scaffold(
+                floatingActionButton = {
+                    FloatingActionButton(onClick = onFabClick) {
+                        Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                    }
+                }
+            ) {paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues))
+                HomeScreen(
+                    sampleSections
+                )
+            }
         }
     }
 }
